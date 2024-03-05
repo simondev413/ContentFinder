36fa7c324441d50408cae56a43a68055c5a3feac
SimonDev413 <devemail413@gmail.com>
2024-03-05 10:59:41 +0100

    'Arquivo principal onde é manipulado os outros modulos'

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import re
+from emailFinder import EmailFinder
+from numberFinder import NumberFinder
+from pprint import pprint
+emails = EmailFinder
+numbers = NumberFinder
 
+email1 = emails.find('Este é o meu email pessoal: simao413@gmail.com, mas para assuntos de trabalho eu uso este: devemail413@gmail.com')
+number1 = numbers.find('Este é o meu número de tefone de Angola: +244 925845239, as veezes uso este para assuntos particulares: +244 947-769-894. Caso nenhum deles passar ligue para este: +23 265 568 323.')
 
-def findNumber(args):
-    regex = re.compile(r"""
-        (\+\d{2,3})? #indicativo
-        (\s|-|\.)? #separador
-        (\d{9}    #Nove digitos, no caso de todos os número serem digitados sem separador
-        |\d{3}    # 3 digitos depois do separador, no caso de houver separador
-        (\s|-|\.) #separador
-        \d{3}    # Mais 3 digitos
-        (\s|-|\.) #separador
-        \d{3}) # Últimos três digitos
-        """,re.VERBOSE)
-    reFound =  regex.findall(args)
-    numbers = {}
-    for number in reFound:
-        if number[0] not in numbers.keys():
-            numbers.setdefault(number[0],[number[2]])
-        else:
-            numbers[number[0]].append(number[2])         
-        
-
-    return numbers
-
-print(findNumber('+244 925-845-239 e +23 925 845 239 e também +244-925845239 e +23 454-845-231'))
+for email in email1:
+    pprint(email)
+for number in number1.items():
+    pprint(number)
